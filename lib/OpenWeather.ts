5259aa2fae0d16ec1453723b89720b3103cb4bf3
Siddhesh Pravin Patil <141564153+siddhesh940@users.noreply.github.com>
2025-12-29 16:20:56 +0530

    Update OpenWeather.ts

--- lib/OpenWeather.ts
+++ lib/OpenWeather.ts
@@ -1,15 +1,11 @@
 import axios from "axios";
 
-
 const OpenWeather = axios.create({
-    baseURL: "https://api.openweathermap.org",
-    params: {
-        appid: process.env.OPENWEATHER_API_KEY,
-        units: "metric",
-    },
+  baseURL: "https://api.openweathermap.org",
+  params: {
+    appid: process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY,
+    units: "metric",
+  },
 });
 
 export default OpenWeather;
-
-
-
